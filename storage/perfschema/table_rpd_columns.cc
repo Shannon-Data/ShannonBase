a54ecbf23f744bb21913c27c714280f054e32ef6
RingsC <hom.lee@hotmail.com>
2024-04-10 11:31:57 +0800

    Remove warnings shannaon (#138)
    * fix(shannon): remove warnings

--- storage/perfschema/table_rpd_columns.cc
+++ storage/perfschema/table_rpd_columns.cc
@@ -149,7 +149,7 @@ int table_rpd_columns::make_row(uint index[[maybe_unused]]) {
     m_row.ndv = ShannonBase::meta_rpd_columns_infos[index].ndv;
     memset(m_row.encoding, 0x0, NAME_LEN);
     strncpy(m_row.encoding, ShannonBase::meta_rpd_columns_infos[index].encoding,
-            strlen(ShannonBase::meta_rpd_columns_infos[index].encoding));
+            sizeof(m_row.encoding));
   }
   return 0;
 }
