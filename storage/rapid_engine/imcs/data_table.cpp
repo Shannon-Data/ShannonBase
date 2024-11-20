a8c26c8349c91da7d579438117d7d88ba12cc3a1
shannon data ai <shannon.data.tech@gmail.com>
2024-11-20 11:07:11 +0800

    feat(rapid):fix uncompressing in lz4

--- storage/rapid_engine/imcs/data_table.cpp
+++ storage/rapid_engine/imcs/data_table.cpp
@@ -166,11 +166,11 @@ start_pos:
     if (is_text_value) {
       uint32 str_id = *reinterpret_cast<uint32 *>(data_ptr);
       auto str_ptr = reinterpret_cast<char *>(cu->header()->m_local_dict->get(str_id));
-      Utils::Util::is_blob(cu->header()->m_type)
-          ? down_cast<Field_blob *>(source_fld)->store(str_ptr, strlen(str_ptr), source_fld->charset())
-          : (Utils::Util::is_varstring(cu->header()->m_source_fld->type())
-                 ? source_fld->store(reinterpret_cast<char *>(str_ptr), strlen(str_ptr), source_fld->charset())
-                 : source_fld->store(reinterpret_cast<char *>(str_ptr), cu->pack_length(), source_fld->charset()));
+      auto len =
+          (Utils::Util::is_blob(cu->header()->m_type) || Utils::Util::is_varstring(cu->header()->m_source_fld->type()))
+              ? strlen(str_ptr)
+              : cu->pack_length();
+      source_fld->store(str_ptr, len, source_fld->charset());
     } else
       source_fld->pack(const_cast<uchar *>(source_fld->data_ptr()), data_ptr, normalized_length);
 
